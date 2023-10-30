4316ef45a6ee7feb481930fd2072915862014ff5
None None <none@none.none>
2023-10-30 19:02:30 +0000

    TEMP commit: hack init.sh to flash the Google logo

--- recipes-core/initrdscripts/initramfs-scripts-android/init.sh
+++ recipes-core/initrdscripts/initramfs-scripts-android/init.sh
@@ -40,7 +40,6 @@ setup_devtmpfs ""
 
 # Checks wether we need to start adbd for interactive debugging
 cat /proc/cmdline | grep debug-ramdisk
-if [ $? -ne 1 ] ; then
     mkdir -p /dev/usb-ffs/adb
     mount -t functionfs adb /dev/usb-ffs/adb
 
@@ -56,8 +55,14 @@ if [ $? -ne 1 ] ; then
     echo 1 > /sys/class/android_usb/android0/enable
 
     /usr/bin/android-gadget-setup adb
-    /usr/bin/adbd
-fi
+    /usr/bin/adbd &
+    while true; do
+	echo 0 > /sys/class/backlight/panel_0/brightness;
+	sleep 1;
+	cat /sys/class/backlight/panel_0/max_brightness > /sys/class/backlight/panel_0/brightness;
+	sleep 1;
+    done &
+    while sleep 1; do : ; done
 
 rotation=0
 if [ -e /etc/rotation ]; then
@@ -74,59 +79,8 @@ fi
 info "Mounting sdcard..."
 mkdir -m 0777 /sdcard /loop
 
-while [ ! -e /dev/$sdcard_partition ] ; do
-    info "Waiting for $sdcard_partition..."
-    sleep 1
-done
-
-/sbin/fsck.ext4 -p /dev/$sdcard_partition
-mount -t auto -o rw,noatime,nodiratime /dev/$sdcard_partition /sdcard
-[ $? -eq 0 ] || fail "Failed to mount the sdcard. Cannot continue."
-
-info "Checking for loop rootfs image on the sdcard..."
-ANDROID_MEDIA_DIR="/sdcard/media/"
-if [ -d /sdcard/media/0 ] ; then
-    ANDROID_MEDIA_DIR="/sdcard/media/0"
-fi
-
-if [ -e /sdcard/asteroidos.ext4 ] ; then
-    ANDROID_MEDIA_DIR="/sdcard/"
-fi
-
-BOOT_DIR="/sdcard"
-if [ -e $ANDROID_MEDIA_DIR/asteroidos.ext4 ] ; then
-    /sbin/fsck.ext4 -p $ANDROID_MEDIA_DIR/asteroidos.ext4
-    # Boots from a /sdcard/asteroidos.ext4 loop file
-    info "Rootfs image found at $ANDROID_MEDIA_DIR/asteroidos.ext4; mounting it now ..."
-    mount -o noatime,nodiratime,sync,rw,loop $ANDROID_MEDIA_DIR/asteroidos.ext4 /loop
-    [ $? -ne 0 ] || BOOT_DIR="/loop"
-fi
-
-if [ ! -e $system_partition ] ; then
-    info "Mounting system..."
-    mkdir -m 0777 $BOOT_DIR/system
-    mount -t auto -o ro /dev/$system_partition $BOOT_DIR/system
-    mount --bind $BOOT_DIR/system /system
-fi
-
-if [ ! -e $vendor_partition ] ; then
-    info "Mounting vendor..."
-    mkdir -m 0777 $BOOT_DIR/vendor
-    mount -t auto -o ro /dev/$vendor_partition $BOOT_DIR/vendor
-    mount --bind $BOOT_DIR/vendor /vendor
-fi
-
-if [ ! -e $firmware_partition ] ; then
-    info "Mounting firmware..."
-    mkdir -m 0777 $BOOT_DIR/firmware
-    mount -t auto -o ro /dev/$firmware_partition $BOOT_DIR/firmware
-    mount --bind $BOOT_DIR/firmware /firmware
-fi
-
-if [ -x /init.machine ]; then
-    info "Run machine specific init"
-    /init.machine $BOOT_DIR > /dev/ttyprintk
-fi
+BOOT_DIR=/sdcard
+mount -t auto -o rw,noatime,nodiratime /dev/mmcblk0p28 /sdcard
 
 setup_devtmpfs $BOOT_DIR
 
@@ -142,4 +96,6 @@ echo FIFO $BOOT_DIR/run > /run/psplash_fifo
 sleep 1
 
 info "Switching to rootfs..."
-exec switch_root -c /dev/ttyprintk $BOOT_DIR /lib/systemd/systemd
+mkdir $BOOT_DIR/initrd
+pivot_root $BOOT_DIR /initrd
+exec /lib/systemd/systemd
